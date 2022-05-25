cd9c87eb667a0774d478f93f5260c78d4f9d782f
rshuttle <reece.shuttle@gmail.com>
2022-05-25 01:05:16 -0400

    beautify

--- code/embedding.py
+++ code/embedding.py
@@ -4,22 +4,25 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import os.path
+from sentence_transformers import util
 
 openai.api_key = "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" #given by OpenAI
 
 courses = ['18.01', '18.02', '18.03', '6.042', '18.05', '18.06', 'COMS3251']
 labels = {'18.01':'r.', '18.02':'g.', '18.03':'b.', '18.05':'mx', '18.06':'k+', '6.042':'cx', 'COMS3251':'y+'}
 embeddings_location = 'code/embeddings.json'
+embedding_engine = 'text-similarity-babbage-001'
+questions_per_course = 25
 
-def make_embeddings(questions_per_course=20, embedding_engine = 'text-similarity-babbage-001'):
+def make_embeddings(embedding_engine, questions_per_course=25):
     """
     Takes json files of questions, embeds them using OpenAI's Babbage embedding engine,
     and saves a new json, embeddings.json, of the embeddings.
     """
     list_of_embeddings = []
     for course in courses:
-        print("Currently embedding: " + course)
-        for num in [i for i in range(1, questions_per_course + 1)]:
+        print("Currently embedding " + course + "...")
+        for num in range(1, questions_per_course + 1):
             if num < 10:
                 number = '0' + str(num)
             else:
@@ -27,50 +30,67 @@ def make_embeddings(questions_per_course=20, embedding_engine = 'text-similarity
             with open('./Data/'+course+'/'+course+'_Question_'+number+'.json', 'r') as f:
                 data = json.load(f)
             raw_question = data['Original question']
-            output = openai.Embedding.create(input = raw_question, engine = embedding_engine)
-            embedding = output['data'][0]['embedding']
+            embedding = openai.Embedding.create(input = raw_question, 
+                                                engine = embedding_engine)['data'][0]['embedding']
+            # embedding = output['data'][0]['embedding']
             list_of_embeddings.append(embedding)
     embeddings = {'list_of_embeddings':list_of_embeddings}
-    # location = 'code/embeddings.json'
     with open(embeddings_location, 'w') as f:
         f.write(json.dumps(embeddings))
     return None
 
 def get_embeddings(embeddings_file):
     """
-    Retrieves embeddings from embeddings_file 
+    Retrieves embeddings from embeddings_file.
     """
     with open(embeddings_file, 'r') as f:
         points = json.load(f)['list_of_embeddings']  #140x2048
     return np.array(points)
 
+def get_most_similar(embeddings, i):
+    """returns most similar questions to the target, index i, via cosine similarity"""
+    cos_sims = []
+    cos_to_num = {}
+    for j in range(len(embeddings)):
+        cos_sim = util.cos_sim(embeddings[i], embeddings[j]).item()
+        cos_to_num[cos_sim] = j
+        cos_sims.append(cos_sim)
+    ordered = sorted(cos_sims, reverse=True)
+    closest_qs = []
+    for val in ordered:
+        closest_qs.append(cos_to_num[val]+1)
+    return closest_qs[1:]
+
 def reduce_via_umap(embeddings, num_dims=2):
     """
     reduces the dimensionality of the provided embeddings to num_dims via UMAP.
-    if embeddings was an n by d np.array, it will be reduced to a n by num_dims np.array
+    if embeddings was an n by d numpy array, it will be reduced to a n by num_dims numpy array.
     """
     reducer = umap.UMAP(n_components=num_dims)
     reduced = reducer.fit_transform(embeddings) #140x2
     return reduced
 
-def plot_clusters(points, questions_per_course=20, question_labels=False, show=False, dpi=200):
+def plot_clusters(points, questions_per_course=25, question_labels=False, show=False, dpi=200):
     """
-    plots clusters of points.
+    plots clusters of points. points is assumed to be a n by 2 numpy array.
     Set question_labels to True if you want to see each point labeled with its question number.
     Set show to True if you want the created plot to pop up.
     """
-    vis_dims = points
-    x = [x for x,y in vis_dims]
-    y = [y for x,y in vis_dims]
-    print(f'points to graph:{len(x)}')
+    x = [x for x,y in points]
+    y = [y for x,y in points]
+    # print(f'points to graph:{len(x)}')
     plt.subplots_adjust(right=0.72)
     figure = plt.gcf()
     figure.set_size_inches(9.5,6.5)
     for i in range(len(courses)):
-        plt.scatter(x[i*questions_per_course:(i+1)*questions_per_course], y[i*questions_per_course:(i+1)*questions_per_course], c=labels[courses[i]][0], label=courses[i], marker = labels[courses[i]][1])
+        plt.scatter(x[i*questions_per_course:(i+1)*questions_per_course], 
+                    y[i*questions_per_course:(i+1)*questions_per_course], 
+                    c = labels[courses[i]][0], 
+                    label = courses[i], 
+                    marker = labels[courses[i]][1])
         if question_labels:
             for j in range(questions_per_course):
-                plt.annotate(j+1, (x[questions_per_course*i+j],y[questions_per_course*i+j]), fontsize='xx-small')
+                plt.annotate(j+1, (x[questions_per_course*i+j], y[questions_per_course*i+j]), fontsize='xx-small')
     plt.legend(bbox_to_anchor=(1, 1.01))
     plt.savefig("UMAP.png", dpi=dpi)
     if show:
@@ -78,7 +98,7 @@ def plot_clusters(points, questions_per_course=20, question_labels=False, show=F
 
 if __name__ == "__main__":
     if not os.path.exists(embeddings_location):
-        make_embeddings()
+        make_embeddings(embedding_engine)
     embeddings = get_embeddings(embeddings_location)
     reduced_points = reduce_via_umap(embeddings)
     plot_clusters(reduced_points, question_labels=True)
--- code/zero-shot.py
+++ code/zero-shot.py
@@ -2,60 +2,67 @@ import openai
 import json
 import pandas as pd
 import time
-from sentence_transformers import util
-from embedding import get_embeddings
+from embedding import get_embeddings, get_most_similar
 
 openai.api_key = "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" #given by OpenAI
-courses = ['18.01']#, '18.02', '18.03', '6.042', '18.05', '18.06', 'COMS3251']
+courses_to_zero_shot = ['18.01']#, '18.02', '18.03', '6.042', '18.05', '18.06', 'COMS3251']
 questions_per_course = 3
 
-def get_most_similar(embeddings, i):
-    """returns most similar questions to the target, index i, via cosine similarity"""
-    #cosine similarity
-    cos_sims = []
-    cos_to_num = {}
-    for j in range(len(embeddings)):
-        cos_sim = util.cos_sim(embeddings[i], embeddings[j]).item()
-        cos_to_num[cos_sim] = j
-        cos_sims.append(cos_sim)
-    ordered = sorted(cos_sims, reverse=True)
-    closest_qs = []
-    for val in ordered:
-        closest_qs.append(cos_to_num[val]+1)
-    # print("Here are the next closest questions to "+str(i+1)+" :"+str(closest_qs[1:]))
-    return closest_qs[1:]
+embedding_location = "code/embeddings.json"
+all_embeddings = get_embeddings(embedding_location)
 
-all_embeddings = get_embeddings("code/embeddings.json")
+docstring_front = '''"""\n''' 
+docstring_back = '''\n"""\n'''
+context_array = ['write a program', 'using sympy', 'using simulations']
+prompt_prefix = 'that answers the following question:'
+explanation_suffix = "\n\n'''\nHere's what the above class is doing:\n1."
 
-#makes input for each prompt
-front_piece = '''"""\n''' #these two lines add triple quotes to the prompt like a doc string
-back_piece = '''\n"""\n'''
-context = 'Write a program, using numpy, sympy and other python libraries, that answers the following question: '
- 
 #makes CSV's for each course:
-for course_index, course in enumerate(courses):
+for course_index, course in enumerate(courses_to_zero_shot):
     course_embeddings = all_embeddings[course_index*questions_per_course:(course_index+1)*questions_per_course]
     questions = []
-    for num in [i for i in range(1, questions_per_course+1)]:
-        if num<10:
-                number='0'+str(num)
+    answers = []
+    for num in range(1, questions_per_course+1):
+        if num < 10:
+                number = '0' + str(num)
         else:
-            number=str(num)
+            number = str(num)
         with open('./Data/'+course+'/'+course+'_Question_'+number+'.json', 'r') as f:
             data = json.load(f)
         raw_question = data['Original question']
+        answer_to_question = data['Program solution']
         questions.append(raw_question)
-    entries = [[i+1,questions[i],front_piece+context+questions[i]+back_piece] for i in range(questions_per_course)]
+        answers.append(answer_to_question)
+
+    rows = []
     for i in range(questions_per_course):
+        question_index = i
+        original_question = questions[i]
+        codex_input = docstring_front + context_array[0] + ' ' + prompt_prefix + ' ' + questions[i] + docstring_back
         start = time.time()
-        print(f"{course} question {i+1}")
-        entries[i].append(openai.Completion.create(engine = "code-cushman-001", prompt = entries[i][2], max_tokens = 256, temperature = 0, top_p = 0)['choices'][0]['text'])
-        entries[i].append(entries[i][3]+"\n\n'''\nHere's what the above class is doing:\n1.")
-        entries[i].append(openai.Completion.create(engine = "code-davinci-002", prompt = entries[i][3]+"\n\n'''\nHere's what the above class is doing:\n1.", max_tokens = 150, temperature = 0, top_p = 0)['choices'][0]['text'])
-        entries[i].append(openai.Completion.create(engine= "text-davinci-002", prompt = entries[i][1], max_tokens = 200, temperature = 0, top_p = 0)['choices'][0]['text'])
-        entries[i].append('')  #actual solution, will do this later
-        entries[i].append('')  #this column is filled in by the person
-        entries[i].append(get_most_similar(course_embeddings,i))
+        print("Running Codex on " + course + " question " + str(i+1) + "...")
+        codex_output = openai.Completion.create(engine = "code-davinci-002", 
+                                                prompt = codex_input, 
+                                                max_tokens = 256, 
+                                                temperature = 0, 
+                                                top_p = 0)['choices'][0]['text']
+        explanation_input = codex_output + explanation_suffix
+        explanation_output = openai.Completion.create(engine = "code-davinci-002", 
+                                                      prompt = explanation_input, 
+                                                      max_tokens = 150, 
+                                                      temperature = 0, 
+                                                      top_p = 0)['choices'][0]['text']
+        gpt3_output = openai.Completion.create(engine = "text-davinci-002", 
+                                               prompt = original_question, 
+                                               max_tokens = 200, 
+                                               temperature = 0, 
+                                               top_p = 0)['choices'][0]['text']
+        question_answer = answers[i]
+        most_similar_questions = get_most_similar(course_embeddings,i)
         print(f'API call time: '+str(time.time()-start))
-    info = pd.DataFrame(entries, columns=['Question', 'Original Question', 'Codex Input', 'Codex Output', 'Codex Explanation Input', 'Codex Explanation', 'GPT-3 Output', 'Actual Solution', 'Codex: Correct or Incorrect?', "Most Similar Questions"])
-    info.to_csv(course+'.csv', index=False)
+        rows.append([question_index, original_question, codex_input, codex_output, explanation_input, 
+                     explanation_output, gpt3_output, question_answer, '', most_similar_questions])
+    info = pd.DataFrame(rows, columns=['Question', 'Original Question', 'Codex Input', 'Codex Output', 'Codex Explanation Input', 
+                                       'Codex Explanation', 'GPT-3 Output', 'Actual Solution', 'Codex: Correct or Incorrect?', "Most Similar Questions"])
+    course_results_location = course + ' results.csv'
+    info.to_csv(course_results_location, index=False)
