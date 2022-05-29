e6e91331f80d8d068d7502902774290e7a3584d7
Iddo Drori <idrori@cs.columbia.edu>
2022-05-29 03:53:26 -0400

    Update figure4.R

--- code/figures/figure4.R
+++ code/figures/figure4.R
@@ -12,11 +12,12 @@ d.mit[1:8,3] <- d.mit[1:8,3] - d.mit[1:8,2]
 d.mit[1:8,5] <- d.mit[1:8,5] - d.mit[1:8,4]
 d.mit_long <- d.mit %>% pivot_longer(names(d.mit)[2:5], names_to = c("Model"), values_to = c("Accuracy"))
 d.mit_long$Course <- factor(d.mit_long$Course, ordered = TRUE)
+d.mit_long$Model <- factor(d.mit_long$Model, ordered = TRUE, levels = c("Codex Few-Shot", "Codex Zero-Shot", "GPT-3 CoT", "GPT-3"))
 
 plot.mit <- d.mit_long %>% ggplot(aes(Course, Accuracy, fill = Model)) +
   geom_col(aes(as.numeric(as.factor(Course)) - 0.2, Accuracy, fill = Model), width = 0.4, position = "stack", data = d.mit_long %>% filter(grepl("GPT-3", Model))) +
   geom_col(aes(as.numeric(as.factor(Course)) + 0.2, Accuracy, fill = Model), width = 0.4, position = "stack", data = d.mit_long %>% filter(grepl("Codex", Model))) +
-  geom_errorbar(aes(x = 8 - 0.2, y = mean, ymin = mean - se, ymax = mean + se), width = 0.1, position = position_dodge(-0.2), data = d.mit_se %>% filter(grepl("GPT-3", Model))) +
+  geom_errorbar(aes(x = 8 - 0.2, y = mean, ymin = mean - se, ymax = mean + se), width = 0.2, position = position_dodge(0.2), data = d.mit_se %>% filter(grepl("GPT-3", Model))) +
   geom_errorbar(aes(x = 8 + 0.2, y = mean, ymin = mean - se, ymax = mean + se), width = 0.2, position = position_dodge(-0.2), data = d.mit_se %>% filter(grepl("Codex", Model))) +
   geom_vline(aes(xintercept = 7.5), linetype = "dotted") +
   scale_fill_manual(values = palette) +
@@ -33,11 +34,12 @@ d.math[1:7,3] <- d.math[1:7,3] - d.math[1:7,2]
 d.math[1:7,5] <- d.math[1:7,5] - d.math[1:7,4]
 d.math_long <- d.math %>% pivot_longer(names(d.math)[2:5], names_to = c("Model"), values_to = c("Accuracy"))
 d.math_long$Topic <- factor(str_wrap(d.math_long$Topic, 10), ordered = TRUE)
+d.math_long$Model <- factor(d.math_long$Model, ordered = TRUE, levels = c("Codex Few-Shot", "Codex Zero-Shot", "GPT-3 CoT", "GPT-3"))
 
 plot.math <- d.math_long %>% ggplot(aes(Topic, Accuracy, fill = Model)) +
   geom_col(aes(as.numeric(as.factor(Topic)) - 0.2, Accuracy, fill = Model), width = 0.4, position = "stack", data = d.math_long %>% filter(grepl("GPT-3", Model))) +
   geom_col(aes(as.numeric(as.factor(Topic)) + 0.2, Accuracy, fill = Model), width = 0.4, position = "stack", data = d.math_long %>% filter(grepl("Codex", Model))) +
-  geom_errorbar(aes(x = 7 - 0.2, y = mean, ymin = mean - se, ymax = mean + se), width = 0.1, position = position_dodge(-0.2), data = d.math_se %>% filter(grepl("GPT-3", Model))) +
+  geom_errorbar(aes(x = 7 - 0.2, y = mean, ymin = mean - se, ymax = mean + se), width = 0.2, position = position_dodge(0.2), data = d.math_se %>% filter(grepl("GPT-3", Model))) +
   geom_errorbar(aes(x = 7 + 0.2, y = mean, ymin = mean - se, ymax = mean + se), width = 0.2, position = position_dodge(-0.2), data = d.math_se %>% filter(grepl("Codex", Model))) +
   geom_vline(aes(xintercept = 6.5), linetype = "dotted") +
   scale_fill_manual(values = palette) +
@@ -49,6 +51,6 @@ plot.math <- d.math_long %>% ggplot(aes(Topic, Accuracy, fill = Model)) +
   theme(legend.title = element_blank(), axis.text.y = element_blank(), axis.text.x = element_text(angle = 60, hjust = 1))
 
 ggarrange(plot.mit, plot.math, labels = c("A", "B"), common.legend = TRUE, widths = c(8, 7), align = "hv")
-s
+
 ggsave("figure4-sr.pdf", width = 10, height = 4)
 
